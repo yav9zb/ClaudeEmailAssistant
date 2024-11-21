79dd1adb8b47cb47f320936cdbeca596b8110e61
Yovanny Vasquez <157414501+yav9zb@users.noreply.github.com>
2024-11-21 13:26:09 -0500

    removed duplicate declaration

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ const limiter = rateLimit({
     max: 100, // limit each IP to 100 requests per windowMs
 });
 
-const timeout = require('connect-timeout');
-
 app.use(limiter);
 app.use(cors());
 app.use(express.json());
